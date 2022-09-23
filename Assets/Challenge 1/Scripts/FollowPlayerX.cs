6baed96d1c73a2d4e4841ec083dc10bb458d33ff
Kings Kariuki <munenekings2@gmail.com>
2022-09-23 08:39:35 +0300

    Initial commit

---
+++ Assets/Challenge 1/Scripts/FollowPlayerX.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowPlayerX : MonoBehaviour
+{
+    //GameObject containing to reference the plane object
+    public GameObject plane;
+
+    // Offset vector variable containing a vector position to elavate the camera
+    public Vector3 offset = new Vector3(30, 0, 10);
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Transform the position of the camera to the plane position plus the offset position
+        transform.position = plane.transform.position + offset;
+    }
+}
---
+++ Assets/Challenge 1/Scripts/PlayerControllerX.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControllerX : MonoBehaviour
+{
+    // Vehicle speed of type float.
+    public float speed = 20.0f;
+
+    //Rotation speed variable, rotates the plane up and down.
+    public float rotationSpeed = 150f;
+
+    //Empty variable to store vertical input
+    public float verticalInput;
+   
+   
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        // get the user's vertical input
+        verticalInput = Input.GetAxis("Vertical");
+
+        // move the plane forward at a constant rate
+        transform.Translate(Vector3.forward * speed * Time.deltaTime);
+
+        // tilt the plane up/down based on up/down arrow keys
+        transform.Rotate(Vector3.left * rotationSpeed * Time.deltaTime * verticalInput);
+
+
+    }
+}
